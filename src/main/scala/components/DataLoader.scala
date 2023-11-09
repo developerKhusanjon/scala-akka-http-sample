36d7ca780ba5c6de63e6de5be969244b1093c1d3
Khusanjon <66946540+developerKhusanjon@users.noreply.github.com>
2023-11-09 22:21:13 +0500

    little fix

--- src/main/scala/components/DataLoader.scala
+++ src/main/scala/components/DataLoader.scala
@@ -4,7 +4,7 @@ package components
 import domains.Models.{Athlete}
 
 class DataLoader {
-  private val khusanjon = Athlete("26031999", "Khusanjon Tuychiboev", position = "Boxer", nation = "Uzbek", 1.78, 75, 24)
+  private val khusanjon = Athlete("26031999", "Khusanjon Tuychiboev", position = "Boxer", nation = "Uzbek", 1.78, 74, 24)
   private val khabib = Athlete("20091988", "Khabib Nurmugamedov", position = "Former UFC fighter", nation = "Russian", 1.78, 70, 35)
   private val mesut = Athlete("15101988", "Mesut O`zil", position = "Footballer", nation = "Turk", 1.8, 68, 35)
 
